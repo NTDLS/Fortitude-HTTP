47816d91928d7f4ea2df25f343b6406baca0fedd
Josh Patterson <josh@ntdls.com>
2018-12-18 12:05:05 -0500

    Added portable configuration files.

--- Setup/Build Portable.bat
+++ Setup/Build Portable.bat
@@ -8,7 +8,8 @@ MD "Output\32-Bit-Portable\Logs"
 MD "Output\32-Bit-Portable\WebSites"
 copy "..\Source\Console\Release Portable\Win32\*.exe" "Output\32-Bit-Portable\" /Y
 copy "..\Source\Service\Release Portable\Win32\*.exe" "Output\32-Bit-Portable\" /Y
-copy "Data\Server\nonconfigured\*.xml" "Output\32-Bit-Portable\Data\" /Y
+copy "Data\Server\Portable\*.xml" "Output\32-Bit-Portable\Data\" /Y
+copy "Data\Server\Portable\32\*.xml" "Output\32-Bit-Portable\Data\" /Y
 copy "Data\Server\*.xml" "Output\32-Bit-Portable\Data\" /Y
 copy "Data\Interface\*.xml" "Output\32-Bit-Portable\Data\" /Y
 XCopy "IndexImages" "Output\32-Bit-Portable\IndexImages\" /E /Y
@@ -24,7 +25,8 @@ MD "Output\64-Bit-Portable\Logs"
 MD "Output\64-Bit-Portable\WebSites"
 copy "..\Source\Console\Release Portable\x64\*.exe" "Output\64-Bit-Portable\" /Y
 copy "..\Source\Service\Release Portable\x64\*.exe" "Output\64-Bit-Portable\" /Y
-copy "Data\Server\nonconfigured\*.xml" "Output\64-Bit-Portable\Data\" /Y
+copy "Data\Server\Portable\*.xml" "Output\64-Bit-Portable\Data\" /Y
+copy "Data\Server\Portable\64\*.xml" "Output\64-Bit-Portable\Data\" /Y
 copy "Data\Server\*.xml" "Output\64-Bit-Portable\Data\" /Y
 copy "Data\Interface\*.xml" "Output\64-Bit-Portable\Data\" /Y
 XCopy "IndexImages" "Output\64-Bit-Portable\IndexImages\" /E /Y
